8d556582834e2bbf6e51b7976a44e2d141ac736f
fang_l <fang_l@worksap.co.jp>
2012-07-20 08:43:25 +0800

    topic01

--- GitAdvancedProject/src/Main.java
+++ GitAdvancedProject/src/Main.java
@@ -7,12 +7,17 @@ public class Main {
 	public static void main(String[] args) {
 		//master01
 		//master02
+<<<<<<< OURS
 		//master03
 		//master04
 		//topic01
 		//topic02
 		//topic03
 		//topic04
+=======
+		//topic01
+
+>>>>>>> THEIRS
 	}
 
 }
